66ff71426f8977daf4ea48aaa4d3837177e12353
liuyu-123-ui <2875728586@qq.com>
2020-10-02 17:48:41 +0800

    first commit

---
+++ bmi.js
@@ -0,0 +1,16 @@
+let markkg;
+markkg = 40;
+let markht;
+markht =1.7;
+let johnkg;
+johnkg = 50;
+let johnht;
+johnht = 1.8;
+mark = markkg/(markht*markht);
+console.log("mark的BMI:",mark);
+john = johnkg/(johnht*johnht);
+console.log("john的BMI:",john);
+console.log("Mark的BMI是否比John更高？");
+console.log("mark" > "john");
+
+
